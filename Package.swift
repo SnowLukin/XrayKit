a5faf12b458f4b7a38c76068408fcc8dc9d69ad2
snowlukin <snowlukin@yandex-team.ru>
2025-10-29 13:15:08 +0300

    Added macos to package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "XrayKit",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v15), .macOS(.v12)],
     products: [
         .library(
             name: "XrayKit",
